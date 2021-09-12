e10a60052bc911a292e46914a8c8ecace560ead2
Wire-less-LAN <1074046068@qq.com>
2021-09-12 15:05:46 +0800

    initial commitment

---
+++ Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creature : MonoBehaviour
+{
+    //values
+    float speed;
+
+    //components
+    Rigidbody2D body;
+
+    void Awake()
+    {
+        //initialize components
+        body = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void move(float angle)
+    {
+        
+    }
+}
